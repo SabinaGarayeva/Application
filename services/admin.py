3d29db12fb5aa7ec50ac67116caddbae06ea5030
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2024-05-08 22:41:51 +0400

    service

--- services/admin.py
+++ services/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import ConnectionSurvey, Internet, Voice, TV, ServiceType
+from .models import Task, Internet, Voice, TV
 
-admin.site.register(ServiceType)
-admin.site.register(ConnectionSurvey)
+admin.site.register(Task)
 admin.site.register(Internet)
 admin.site.register(Voice)
 admin.site.register(TV)
--- services/models.py
+++ services/models.py
@@ -1,43 +1,45 @@
 from django.db import models
 
-SERVICE_TYPES = (
-    ('Internet', 'Internet'),
-    ('TV', 'TV'),
-    ('Voice', 'Voice'),
+TASK_TYPES = (
+    ('connection', 'connection'),
+    ('problem', 'problem'),
 )
 
-class ServiceType(models.Model):
-    name = models.CharField(max_length=100, choices=SERVICE_TYPES)
 
-    def __str__(self):
-        return self.name
-
-class ConnectionSurvey(models.Model):
+class Task(models.Model):
+    task_type = models.CharField(max_length=100, choices=TASK_TYPES)
     registration_number = models.CharField(max_length=100)
     contact_number = models.CharField(max_length=100)
-    photo_ID = models.ImageField(upload_to='documents/')
-    location = models.CharField(max_length=100)
-    service_type = models.ManyToManyField('ServiceType')
+    photo_ID = models.ImageField(upload_to='photo_id/')
+    location = models.CharField(max_length=100)  
+
+    def __str__(self):
+        return self.task_type
+
+
 
 class Internet(models.Model):
-    service_type = models.CharField(max_length=100, choices=SERVICE_TYPES)
-    photo_modem = models.ImageField(upload_to='documents/')
+    task = models.OneToOneField(Task, on_delete=models.CASCADE, related_name='internet')
+    photo_modem = models.ImageField(upload_to='internet/')
     modem_SN = models.CharField(max_length=100)
     optical_cable = models.CharField(max_length=100)
     fastconnector = models.CharField(max_length=100)
     siqnal = models.CharField(max_length=100)
 
+
 class TV(models.Model):
-    service_type = models.CharField(max_length=100, choices=SERVICE_TYPES)
-    photo_modem = models.ImageField(upload_to='documents/')
+    task = models.OneToOneField(Task, on_delete=models.CASCADE, related_name='tv')
+    photo_modem = models.ImageField(upload_to='tv/')
     modem_SN = models.CharField(max_length=100)
     rg6_cable = models.CharField(max_length=100)
     f_connector = models.CharField(max_length=100)
     splitter = models.CharField(max_length=100)
 
+
 class Voice(models.Model):
-    service_type = models.CharField(max_length=100, choices=SERVICE_TYPES)
-    photo_modem = models.ImageField(upload_to='documents/')
+    task = models.OneToOneField(Task, on_delete=models.CASCADE, related_name='voice')
+    photo_modem = models.ImageField(upload_to='voice/')
     modem_SN = models.CharField(max_length=100)
     home_number = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+
