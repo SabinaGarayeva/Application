1405e8d212ff0125f7f3baf7e4579e8d5b1b69e6
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2024-05-12 17:00:13 +0400

    completed_count

--- accounts/models.py
+++ accounts/models.py
@@ -45,7 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username", "first_name", "last_name", "phone", "user_type"]
+    REQUIRED_FIELD = "username"
 
     def tokens(self):    
         refresh = RefreshToken.for_user(self)
@@ -94,3 +94,5 @@ class OneTimePassword(models.Model):
 
     def __str__(self):
         return f"{self.user.first_name} - otp code"
+    
+    
--- services/views.py
+++ services/views.py
@@ -4,6 +4,8 @@ from .filters import StatusAndTaskFilter
 from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import status
+from rest_framework.response import Response
 
 class CreateTaskView(generics.CreateAPIView):
     serializer_class = TaskSerializer
@@ -25,3 +27,19 @@ class TaskListAPIView(generics.ListAPIView):
     filterset_class = StatusAndTaskFilter
     filter_backends = (DjangoFilterBackend,)
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        connection_completed_count = queryset.filter(task_type='connection', status='completed').count()
+        problem_completed_count = queryset.filter(task_type='problem', status='completed').count()
+
+        context = {
+            'connection_completed_count': connection_completed_count,
+            'problem_completed_count': problem_completed_count
+        }
+
+        serializer = self.get_serializer(queryset, many=True)
+        response_data = serializer.data
+        response_data.append(context)
+
+        return Response(response_data, status=status.HTTP_200_OK)
