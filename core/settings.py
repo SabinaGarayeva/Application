7be3bce2150424feabb572c0c4591bd9a5d7b832
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2024-05-12 13:54:47 +0400

    static

--- core/settings.py
+++ core/settings.py
@@ -69,6 +69,9 @@ INSTALLED_APPS = [
     "services",
 ]
 
+SITE_ID = 1
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
