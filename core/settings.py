bd2faaeb299992f51e638d809083e4210697570a
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2024-05-12 13:33:53 +0400

    static

--- core/settings.py
+++ core/settings.py
@@ -177,12 +177,12 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/mediafiles/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles/')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Default primary key field type
