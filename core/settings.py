67477af0b76bc741bdafa155b6283228347eac88
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2024-05-16 02:10:35 +0400

    del

--- core/settings.py
+++ core/settings.py
@@ -250,7 +250,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
 
 
 # Default primary key field type
